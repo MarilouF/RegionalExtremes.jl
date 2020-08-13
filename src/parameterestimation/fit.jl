cac0a9dcc7280aafe6ce48f8064f6051cccf80ee
MarilouF <farmer.marilou@hotmail.fr>
2020-08-13 14:00:27 -0400

    Add acc rate check

--- src/parameterestimation/fit.jl
+++ src/parameterestimation/fit.jl
@@ -21,5 +21,12 @@ function fit(m::Tuple{Integer, Integer}, p::Integer, y::Array{<:Real, 2},
 
     run!(chain)
 
+    accrate = mean(chain.θacc[chain.burnin:end, :, :], dims=(1,3))[:, :, 1]'[:, 1]
+    notrecommended = (0.2 .>= accrate) .| (accrate .>= 0.3)
+
+    if any(notrecommended)
+        @warn "Parameter(s) $(collect(1:size(chain.θacc, 2))[notrecommended]) acceptance rate $(round.(accrate[notrecommended], digits = 3)) not between 20% and 30%. Consider changing the step."
+    end
+
     return chain
 end
--- test/parameterestimation/fit_test.jl
+++ test/parameterestimation/fit_test.jl
@@ -1,7 +1,8 @@
 @testset "fit.jl" begin
     @testset "fit(m, p, y, k, burnin, step; μcov, ϕcov, ξcov, regional)" begin
-        # Calling fit calls run!
-        chain = RegionalExtremes.fit((2,2), 1, [1 0; 1 0; 1 0; 1 0], 4, 2, [0.5, 0.5, 0.5])
+        # Calling fit with inappropriate step warns
+        chain = nothing
+        @test_logs (:warn,) chain = RegionalExtremes.fit((2,2), 1, [1 0; 1 0; 1 0; 1 0], 4, 2, [0.1, 0.1, 0.1])
 
         # θ has values
         @test length(filter(p->p != 0, chain.θ)) > 0
@@ -11,5 +12,10 @@
 
         # κ has values
         @test length(filter(p->p != 0, chain.κ)) > 0
+
+        # Calling fit with appropriate step does not warn
+        κs = [50.0, 50.0, 3000.0]
+        g, θ, x = genGrid((10, 12), 1, κs..., 300)
+        @test_logs RegionalExtremes.fit(g.G.gridSize, g.p, g.y, 500, 250, [0.2, 0.15, 0.08])
     end
 end
